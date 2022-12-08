11c267102b87e04d5daa2945eafe98066d814a31
zsofi82 <zsofigaal@gmail.com>
2022-12-08 11:49:35 +0100

    add @PostConstructor annotation for init method and added two extra residents to their rooms

--- src/main/java/com/codecool/hogwartshouses/data_sample/RoomCreator.java
+++ src/main/java/com/codecool/hogwartshouses/data_sample/RoomCreator.java
@@ -2,8 +2,10 @@ package com.codecool.hogwartshouses.data_sample;
 
 import com.codecool.hogwartshouses.model.Room;
 import com.codecool.hogwartshouses.model.Student;
+import com.codecool.hogwartshouses.model.types.HouseType;
 import com.codecool.hogwartshouses.model.types.PetType;
 import com.codecool.hogwartshouses.repository.RoomMemory;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -17,21 +19,21 @@ public class RoomCreator {
     @Autowired
     public RoomCreator(RoomMemory roomMemory) {
         this.roomMemory = roomMemory;
-        initialize();
     }
-
+    @PostConstruct
     public void initialize() {
-        Student Hermione = Student.builder().id(UUID.randomUUID()).name("Hermione Granger").petType(PetType.CAT).build();
-        Student Draco = Student.builder().id(UUID.randomUUID()).name("Draco Malfoy").petType(PetType.OWL).build();
-        Student Luna = Student.builder().id(UUID.randomUUID()).name("Luna Lovegood").petType(PetType.NONE).build();
-        Student Neville = Student.builder().id(UUID.randomUUID()).name("Neville Longbottom").petType(PetType.TOAD).build();
+        Student Hermione = Student.builder().name("Hermione Granger").petType(PetType.CAT).build();
+        Student Draco = Student.builder().name("Draco Malfoy").petType(PetType.OWL).build();
+        Student Luna = Student.builder().name("Luna Lovegood").petType(PetType.NONE).build();
+        Student Neville = Student.builder().name("Neville Longbottom").petType(PetType.TOAD).build();
 
-        Room room1 = Room.builder().id(1L).capacity(2).resident(Luna).build();
-        Room room2 = Room.builder().id(2L).capacity(2).resident(Neville).build();
-        Room room3 = Room.builder().id(3L).capacity(2).build();
+        Room room1 = Room.builder().capacity(2).houseType(HouseType.GRYFFINDOR).resident(Luna).build();
+        Room room2 = Room.builder().capacity(2).houseType(HouseType.HUFFLEPUFF).resident(Neville).build();
+        Room room3 = Room.builder().capacity(2).houseType(HouseType.RAVENCLAW).resident(Hermione).build();
+        Room room4 = Room.builder().capacity(2).houseType(HouseType.SLYTHERIN).resident(Draco).build();
         roomMemory.createRoom(room1);
         roomMemory.createRoom(room2);
         roomMemory.createRoom(room3);
-
+        roomMemory.createRoom(room4);
     }
 }
